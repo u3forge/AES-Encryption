7052d7d6e20b723f2cb3d17a45bf239a6eaf4536
Ahmed Elmayyah <a.elmayyah@gmail.com>
2019-12-18 12:05:11 +0200

    Split project into files, fixed WriteHex problems.

--- AES-Encryption.asm
+++ AES-Encryption.asm
@@ -1,104 +1,21 @@
 ; Assembly with  nasm -f elf AES-Encryption.asm
 ; Link with ld -m elf_i386 AES-Encryption.o -o AES-Encryption
 ; Or just run make like a chad
+%include "IO.inc" ; Custom Written IO library
+%include "SBOX.inc" ; File contining SBOX
+
 global _start
 section .data:
 	val db	0x41
-	st db "TEST", 0x0
+	st db "TEST2", 0x0
 section .text:
 _start:
-	mov esi, st
-	mov ecx, 5
-	call WriteString
-
-	mov al, 0xA
-	call WriteChar
-
-	mov eax, 123456
-	call WriteDec
-
-	mov al, 0xA
-	call WriteChar
 	
-	mov eax, 0x190
+	mov al, [SBOX+1]
 	call WriteHex
 
-	mov al, 0xA
-	call WriteChar
 
 	;Return zero
 	mov eax, 0x1
 	mov ebx, 0
 	int 0x80
-
-WriteChar:
-	push eax       	;Store parameter on the stack
-	mov ecx, esp	;Get address of the parameter
-	mov eax, 0x4	;Specifying the write syscall
-	mov ebx, 0x1	;STDOUT file descriptor
-	mov edx, 1		;Printing one byte
-	int 0x80		;Invoke the syscall
-	pop eax			;Clean up the stack
-	ret
-
-WriteString:
-	.print:
-	mov eax, [esi]
-	push ecx
-	call WriteChar
-	pop ecx
-	inc esi
-	loop .print
-	ret
-
-WriteDec:
-	push 0xf1a9
-	.next:
-		mov ecx, 10
-		mov edx, 0
-		div ecx
-		add dl, 0x30
-		mov edi, eax
-		xor eax, eax
-		mov al, dl
-		push eax
-		mov eax, edi
-	cmp eax, 0
-	jne .next
-	.print:
-		pop eax
-		cmp eax, 0xf1a9
-		je .done
-		call WriteChar
-		jmp .print
-	.done:
-	ret
-
-WriteHex:
-	push eax
-	mov al, '0'
-	call WriteChar
-	mov al, 'x'
-	call WriteChar
-	pop eax
-	push 0xf1a9
-	.next:
-		mov ecx, 16
-		mov edx, 0
-		div ecx
-		add dl, 0x30
-		mov edi, eax
-		xor eax, eax
-		mov al, dl
-		push eax
-		mov eax, edi
-	cmp eax, 0
-	jne .next
-	.print:
-		pop eax
-		cmp eax, 0xf1a9
-		je .done
-		call WriteChar
-		jmp .print
-	.done:
-	ret
--- makefile
+++ makefile
@@ -1,3 +1,3 @@
-AES-Encryption: AES-Encryption.asm
+AES-Encryption: AES-Encryption.asm SBOX.inc IO.inc
 	nasm -f elf AES-Encryption.asm
 	ld -m elf_i386 AES-Encryption.o -o AES-Encryption
