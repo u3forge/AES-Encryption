9661925b1b6f56f7f387fc3ef7a534538577503b
Ahmed Elmayyah <a.elmayyah@gmail.com>
2019-12-22 23:11:19 +0200

    Finished encryption.

--- AES-Encryption.asm
+++ AES-Encryption.asm
@@ -2,44 +2,24 @@
 ; Link with ld -m elf_i386 AES-Encryption.o -o AES-Encryption
 ; Or just run make like a chad
 %include "IO.inc" 			; Custom Written IO library
-%include "Substitution.inc"	; Substitution functions
-%include "Rotation.inc"		; Rotation functions
-%include "ColumnMixing.inc"	; Column mixing functions
-%include "RoundKey.inc"		; Round key generation functions
+%include "Encryption.inc"	; Encryption functions
 global _start
 
 section .data
-message db 0x19, 0xa0, 0x9a, 0xe9, 0x3d, 0xf4, 0xc6, 0xf8, 0xe3, 0xe2, 0x8d, 0x48, 0xbe, 0x2b, 0x2a, 0x08
+message db 0x32, 0x88, 0x31, 0xe0, 0x43, 0x5a, 0x31, 0x37, 0xf6, 0x30, 0x98, 0x07, 0xa8, 0x8d, 0xa2, 0x34
 key		db 0x2b, 0x28, 0xab, 0x09, 0x7e, 0xae, 0xf7, 0xcf, 0x15, 0xd2, 0x15, 0x4f, 0x16, 0xa6, 0x88, 0x3c
-SubstitutedMessage TIMES 16 db 0
-mixedMessage TIMES 16 db 0
 
 section .text
 _start:
 
-	mov esi, key
-	call CreateRoundKeys
-	mov esi, RoundKeys
-	call PrintRoundKeys
-
-	mov ecx, 16
 	mov esi, message
-	mov edi, SubstitutedMessage
-	call SubstituteMessage
-	
-	mov esi, SubstitutedMessage
-	call RotateMessage
+	mov edi, key
+	call Encrypt
+
+	mov esi, message
+	mov ecx, 16
 	call Print4x4Matrix
-
-	call WriteLine
 	
-	mov esi, SubstitutedMessage
-	mov edi, mixedMessage
-	call MixColumns
-
-	mov esi, mixedMessage
-	call Print4x4Matrix
-
 	;Return zero
 	mov eax, 0x1
 	mov ebx, 0
